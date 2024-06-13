8703cf0dabcf62fc8439e60d84132483d3db8c57
ishankashyap <ikashyap@ualberta.ca>
2024-06-13 16:12:22 -0700

    changed divs to buttons

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const container = document.querySelector("#container");
+// const container = document.querySelector("#container");
 
-for(let i = 1; i<=5; i++){
-    for(let j = 1; j<=4;j++){
-        const column = document.createElement("div");
-        if(i ==5 && j ==1){
-            column.classList.add("square17");
-        }else if(i ==5 && j ==2){
-            continue;
-        }else{
-            column.className = "squares";
-        }
-        container.appendChild(column);
-    }
-}
+// for(let i = 1; i<=5; i++){
+//     for(let j = 1; j<=4;j++){
+//         const column = document.createElement("div");
+//         if(i ==5 && j ==1){
+//             column.classList.add("square17");
+//         }else if(i ==5 && j ==2){
+//             continue;
+//         }else{
+//             column.className = "squares";
+//         }
+//         container.appendChild(column);
+//     }
+// }
+
